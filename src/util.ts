1ae633be9a0c61ab176c021ba9dd4dbe3508e742
Ivan Preocanin <ivan.preocanin@gmail.com>
2022-04-27 19:44:40 +0200

    API endpoints

--- src/util.ts
+++ src/util.ts
@@ -2,9 +2,15 @@ export const ApiEndpoint = Object.freeze({
   SCHEDULE: {
     BASE: '/schedules',
     FIND_ALL: '/find-all',
+    FIND_ONE: '/find-one',
     CREATE: '/create',
+    DELETE: '/delete',
   },
   TASK: {
     BASE: '/tasks',
+    FIND_BY_SCHEDULE_ID: '/find-by-schedule-id',
+    FIND_ONE: '/find-one',
+    CREATE: '/create',
+    DELETE: '/delete',
   },
 });

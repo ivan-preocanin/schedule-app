a0fdcbcbabfa9f20eb69b1ff8f544c6b6035b869
Ivan Preocanin <ivan.preocanin@gmail.com>
2022-04-27 19:08:18 +0200

    Import fixed

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,5 +1,5 @@
 import { Controller } from '@nestjs/common';
-import { ApiEndpoint } from 'src/util';
+import { ApiEndpoint } from '../util';
 import { TaskService } from './task.service';
 
 @Controller(ApiEndpoint.TASK.BASE)

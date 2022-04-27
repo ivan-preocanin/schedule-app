0782f5cc9eeca99644a857031fedcdbcf42c4317
Ivan Preocanin <ivan.preocanin@gmail.com>
2022-04-27 19:04:37 +0200

    Connection removed from App module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Connection } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Schedule } from './schedule/schedule.entity';
@@ -28,6 +27,4 @@ import { TaskModule } from './task/task.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {
-  constructor(private connection: Connection) {}
-}
+export class AppModule {}

01a5e33ba2da244770c74a8cc49168f9161fe239
Ivan Preocanin <ivan.preocanin@gmail.com>
2022-04-27 19:45:01 +0200

    Task routes and methods

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,8 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Query } from '@nestjs/common';
 import { ApiEndpoint } from '../util';
+import { Task } from './task.entity';
 import { TaskService } from './task.service';
 
 @Controller(ApiEndpoint.TASK.BASE)
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
+
+  @Get(ApiEndpoint.TASK.FIND_ONE)
+  findOne(@Query('id') id: string): Promise<Task> {
+    return this.taskService.findOne(id);
+  }
+
+  @Get(ApiEndpoint.TASK.FIND_BY_SCHEDULE_ID)
+  findByScheduleId(@Query('scheduleId') scheduleId: string): Promise<Task[]> {
+    return this.taskService.findByScheduleId(scheduleId);
+  }
+
+  @Post(ApiEndpoint.TASK.CREATE)
+  create(@Body() schedule: Task): Promise<Task> {
+    return this.taskService.create(schedule);
+  }
+
+  @Delete(ApiEndpoint.TASK.DELETE)
+  delete(@Query('id') id: string): Promise<void> {
+    return this.taskService.delete(id);
+  }
 }
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -9,4 +9,20 @@ export class TaskService {
     @InjectRepository(Task)
     private taskRepository: Repository<Task>,
   ) {}
+
+  findByScheduleId(scheduleId: string): Promise<Task[]> {
+    return this.taskRepository.findBy({ schedule_id: scheduleId });
+  }
+
+  findOne(id: string): Promise<Task> {
+    return this.taskRepository.findOneBy({ id });
+  }
+
+  async delete(id: string): Promise<void> {
+    await this.taskRepository.delete(id);
+  }
+
+  async create(task: Task): Promise<Task> {
+    return await this.taskRepository.save(task);
+  }
 }

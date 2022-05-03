efe802f2a1a523355a43be7a5aa86d9a5eb97417
Ivan Preocanin <ivan.preocanin@gmail.com>
2022-05-03 11:49:00 +0200

    Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,25 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import 'reflect-metadata';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Schedule App')
+    .setDescription('The Schedule API description')
+    .setVersion('1.0')
+    .addTag('schedule')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
 }
 bootstrap();
